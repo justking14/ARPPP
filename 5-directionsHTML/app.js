fe5d13ba70cca30e067aeeaa035e93a2d59ccf1f
Justin Buergi <jhb14@optonline.net>
2016-09-21 18:53:11 -0400

    MASTER

---
+++ 5-directionsHTML/app.js
@@ -0,0 +1,212 @@
+/// <reference path="../../typings/index.d.ts"/>
+// set up Argon
+var app = Argon.init();
+// set up THREE.  Create a scene, a perspective camera and an object
+// for the user's location
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera();
+var userLocation = new THREE.Object3D;
+scene.add(camera);
+scene.add(userLocation);
+// The CSS3DArgonRenderer supports mono and stereo views, and 
+// includes both 3D elements and a place to put things that appear 
+// fixed to the screen (heads-up-display) 
+var renderer = new THREE.CSS3DArgonRenderer();
+app.view.element.appendChild(renderer.domElement);
+// to easily control stuff on the display
+var hud = new THREE.CSS3DArgonHUD();
+// We put some elements in the index.html, for convenience. 
+// Here, we retrieve the description box and move it to the 
+// the CSS3DArgonHUD hudElements[0].  We only put it in the left
+// hud since we'll be hiding it in stereo
+var description = document.getElementById('description');
+hud.hudElements[0].appendChild(description);
+app.view.element.appendChild(hud.domElement);
+// Tell argon what local coordinate system you want.  The default coordinate
+// frame used by Argon is Cesium's FIXED frame, which is centered at the center
+// of the earth and oriented with the earth's axes.  
+// The FIXED frame is inconvenient for a number of reasons: the numbers used are
+// large and cause issues with rendering, and the orientation of the user's "local
+// view of the world" is different that the FIXED orientation (my perception of "up"
+// does not correspond to one of the FIXED axes).  
+// Therefore, Argon uses a local coordinate frame that sits on a plane tangent to 
+// the earth near the user's current location.  This frame automatically changes if the
+// user moves more than a few kilometers.
+// The EUS frame cooresponds to the typical 3D computer graphics coordinate frame, so we use
+// that here.  The other option Argon supports is localOriginEastNorthUp, which is
+// more similar to what is used in the geospatial industry
+app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
+// creating 6 divs to indicate the x y z positioning
+
+var divZneg = document.createElement('div');
+// programatically create a stylesheet for our direction divs
+// and add it to the document
+var style = document.createElement("style");
+style.type = 'text/css';
+document.head.appendChild(style);
+var sheet = style.sheet;
+sheet.insertRule("\n    .cssContent {\n        opacity: 0.5;\n        width: 100px;\n        height: 100px;\n        border-radius: 50%;\n        line-height: 100px;\n        fontSize: 20px;\n        text-align: center;\n    }\n", 0);
+// Put content in each one  (should do this as a couple of functions)
+// for X
+
+divZneg.className = "cssContent";
+divZneg.style.backgroundColor = "green";
+divZneg.innerText = "Neg Z = North";
+// create 6 CSS3DObjects in the scene graph.  The CSS3DObject object 
+// is used by the CSS3DArgonRenderer. Because an HTML element can only
+// appear once in the DOM, we need two elements to create a stereo view.
+// The CSS3DArgonRenderer manages these for you, using the CSS3DObject.
+// You can pass a single DIV to the CSS3DObject, which
+// will be cloned to create a second matching DIV in stereo mode, or you
+// can pass in two DIVs in an array (one for the left and one for the 
+// right eyes).  If the content of the DIV does not change as the 
+// application runs, letting the CSS3DArgonRenderer clone them is 
+// simplest.  If it is changing, passing in two and updating both
+// yourself is simplest.
+
+var bridget01 = new Audio("audio/Chapel_soundbite.mp3");
+var bridget02 = new Audio("audio/Bridget02.mp3");
+var bridget03 = new Audio("audio/Bridget03.mp3");
+
+// Images of people and objects
+var imgDirectory = "images/";	// Image directory path relative to directory containing index.html.
+var bridgetPicture = imgDirectory + "housemaid3.png";
+
+// Buttons
+var buttonDirectory = imgDirectory + "Buttons/";
+var toBridget01 = buttonDirectory + "Sydney1.png";
+var toBridget02 = buttonDirectory + "Sydney2.png";
+var ok = buttonDirectory + "OK.png";
+var cont = buttonDirectory + "Continue.png";
+
+// Logic control
+var currentButton = "";
+var cook01Complete = false;
+var teacupComplete = false;
+var bridgetComplete = false;
+// ------------------------------------------------------------------------------------------------
+
+
+// ------------------------------------------------------------------------------------------------
+// SCENES WITH BUTTONS
+// ------------------------------------------------------------------------------------------------
+// Each scene with buttons runs one function for each button press plus one.
+// 
+// Bridget:
+function setupDishPantry(){
+    var imageElement = document.getElementById("currentImage");
+    imageElement.src = "Local Wedding.png";
+
+    bridget01.play();
+			
+    imageElement.height = "500";
+    imageElement.style.visibility = "visible";
+		
+    //var btn = document.getElementById("dialogButton");
+    //btn.src = toBridget01;
+
+    setTimeout(function(){
+	currentButton = "toBridget01";
+	//btn.style.visibility = "visible";
+
+    }, ((bridget01.duration * 1000) + 300));
+    
+    //setTimeout(askAboutWork(), 20000);
+			setTimeout(function() { askAboutWork(); }, 14500);
+
+}
+ setupDishPantry()
+
+function askAboutWork(){
+    var imageElement = document.getElementById("currentImage");
+    imageElement.src = "Market.png";
+
+    bridget02.play();
+			
+    imageElement.height = "500";
+    imageElement.style.visibility = "visible";
+		
+    //var btn = document.getElementById("dialogButton");
+    //btn.src = toBridget01;
+
+    setTimeout(function(){
+	currentButton = "toBridget01";
+	//btn.style.visibility = "visible";
+    }, ((bridget01.duration * 1000) + 300));
+			
+    bridgetComplete = true;
+}
+
+
+function buttonPressed(){
+	//if(currentButton == "toBridget01") {askAboutWork();}
+	//askAboutWork()
+}
+
+ 
+
+
+// the updateEvent is called each time the 3D world should be
+// rendered, before the renderEvent.  The state of your application
+// should be updated here.
+app.updateEvent.addEventListener(function () {
+    // get the position and orientation (the "pose") of the user
+    // in the local coordinate frame.
+    var userPose = app.context.getEntityPose(app.context.user);
+    // assuming we know the user's pose, set the position of our 
+    // THREE user object to match it
+    if (userPose.poseStatus & Argon.PoseStatus.KNOWN) {
+        userLocation.position.copy(userPose.position);
+    }
+});
+// for the CSS renderer, we want to use requestAnimationFrame to 
+// limit the number of repairs of the DOM.  Otherwise, as the 
+// DOM elements are updated, extra repairs of the DOM could be 
+// initiated.  Extra repairs do not appear to happen within the 
+// animation callback.
+var viewport = null;
+var subViews = null;
+var rAFpending = false;
+app.renderEvent.addEventListener(function () {
+    // only schedule a new callback if the old one has completed
+    if (!rAFpending) {
+        rAFpending = true;
+        viewport = app.view.getViewport();
+        subViews = app.view.getSubviews();
+        window.requestAnimationFrame(renderFunc);
+    }
+});
+// the animation callback.  
+function renderFunc() {
+    // if we have 1 subView, we're in mono mode.  If more, stereo.
+    var monoMode = subViews.length == 1;
+    rAFpending = false;
+    // set the renderer to know the current size of the viewport.
+    // This is the full size of the viewport, which would include
+    // both views if we are in stereo viewing mode
+    renderer.setSize(viewport.width, viewport.height);
+    hud.setSize(viewport.width, viewport.height);
+    // there is 1 subview in monocular mode, 2 in stereo mode
+    for (var _i = 0, subViews_1 = subViews; _i < subViews_1.length; _i++) {
+        var subview = subViews_1[_i];
+        // set the position and orientation of the camera for 
+        // this subview
+        camera.position.copy(subview.pose.position);
+        camera.quaternion.copy(subview.pose.orientation);
+        // the underlying system provide a full projection matrix
+        // for the camera.  Use it, and then update the FOV of the 
+        // camera from it (needed by the CSS Perspective DIV)
+        camera.projectionMatrix.fromArray(subview.projectionMatrix);
+        camera.fov = subview.frustum.fovy * 180 / Math.PI;
+        // set the viewport for this view
+        var _a = subview.viewport, x = _a.x, y = _a.y, width = _a.width, height = _a.height;
+        renderer.setViewport(x, y, width, height, subview.index);
+        // render this view.
+        renderer.render(scene, camera, subview.index);
+        // adjust the hud, but only in mono
+        if (monoMode) {
+            hud.setViewport(x, y, width, height, subview.index);
+            hud.render(subview.index);
+        }
+    }
+}
